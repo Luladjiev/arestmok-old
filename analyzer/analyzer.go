dc45e60ad81b3a183fa6661605226a8ecfcf0014
Peter Luladjiev <luladjiev@gmail.com>
2017-03-04 12:05:42 +0200

    Update analyzer to work with array type

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -1,34 +1,57 @@
 package analyzer
 
-import (
-	"fmt"
-	"math"
-)
+import "math"
+import "fmt"
+import "strconv"
 
-func analyzeData(data map[string]interface{}) {
+func getType(data interface{}) string {
+	switch data.(type) {
+	case int, int8, int16, int32, int64:
+		return "int"
+	case float32, float64:
+		if data == math.Trunc(data.(float64)) {
+			return "int"
+		}
+		return "float"
+	case string:
+		return "string"
+	case bool:
+		return "bool"
+	case []interface{}:
+		return "array"
+	case interface{}:
+		return "object"
+	default:
+		return "unknown"
+	}
+}
+
+func analyzeData(data map[string]interface{}, property string) {
 	for k, v := range data {
-		switch v.(type) {
-		case int, int8, int16, int32, int64:
-			fmt.Println(k, v, "is int")
-		case float32, float64:
-			if v == math.Trunc(v.(float64)) {
-				fmt.Println(k, v, "is int")
-			} else {
-				fmt.Println(k, v, "is float64")
+		if property != "" {
+			k = property + "." + k
+		}
+
+		t := getType(v)
+
+		fmt.Println(k, v, t)
+
+		switch t {
+		case "array":
+			for i, e := range v.([]interface{}) {
+				elementType := getType(e)
+				fmt.Println(k, i, e, elementType)
+				if elementType == "array" || elementType == "object" {
+					analyzeData(e.(map[string]interface{}), k+"["+strconv.Itoa(i)+"]")
+				}
 			}
-		case string:
-			fmt.Println(k, v, "is string")
-		case bool:
-			fmt.Println(k, v, "is bool")
-		case interface{}:
-			analyzeData(v.(map[string]interface{}))
-		default:
-			fmt.Println(k, v, "is unknown")
+		case "object":
+			analyzeData(v.(map[string]interface{}), k)
 		}
 	}
 }
 
 // Analyze data and returns structure with the corresponding types
 func Analyze(data map[string]interface{}) {
-	analyzeData(data)
+	analyzeData(data, "")
 }
